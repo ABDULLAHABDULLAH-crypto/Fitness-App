8183b183495b1334def76d7bb5441c99e4e544da
ABDULLAHABDULLAH-crypto <74300140+ABDULLAHABDULLAH-crypto@users.noreply.github.com>
2024-12-08 23:18:48 +0300

    first commit

---
+++ lib/pages/auth/SignUp.dart
@@ -0,0 +1,158 @@
+import 'package:fitness_app/pages/mainPage.dart';
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(color: Color(0xFF0D1B2A)),
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 0, vertical: 0)),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Image.asset("assets/images/Back.png")),
+          ),
+          SizedBox(height: 30),
+          const Center(
+            child: Text(
+              "SIGN UP",
+              style: TextStyle(color: Colors.white, fontSize: 60),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: Container(
+                width: 500,
+                height: 400,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(20))),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        SizedBox(height: 40),
+                        Padding(
+                          padding: EdgeInsets.all(20),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: "Enter Your Name",
+                                border: OutlineInputBorder(),
+                                icon: Icon(Icons.person)),
+                            // The validator receives the text that the user has entered.
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(20),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: "Enter Your Email",
+                                border: OutlineInputBorder(),
+                                icon: Icon(Icons.person)),
+                            // The validator receives the text that the user has entered.
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(20),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: "Enter Your Password",
+                                border: OutlineInputBorder(),
+                                icon: Icon(Icons.lock)),
+                            // The validator receives the text that the user has entered.
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(20),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: "Re Enter Your Password",
+                                border: OutlineInputBorder(),
+                                icon: Icon(Icons.lock)),
+                            // The validator receives the text that the user has entered.
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 30, horizontal: 120),
+                              backgroundColor:const Color.fromARGB(255, 28, 47, 68)),
+                          onPressed: () {
+                            // Validate returns true if the form is valid, or false otherwise.
+                            if (_formKey.currentState!.validate()) {
+                              // If the form is valid, display a snackbar. In the real world,
+                              // you'd often call a server or save the information in a database.
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Processing Data')),
+                              );
+                            }
+                          },
+                          child: const Text(
+                            'SignUp',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        GestureDetector(
+                          onTap: () {
+                            // Handle Skip action
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MainPage()));
+                          },
+                          child: const Text(
+                            "Skip",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ))),
+          )
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/pages/onBoarding.dart
@@ -0,0 +1,148 @@
+import 'package:fitness_app/pages/auth/SignIn.dart';
+import 'package:fitness_app/pages/auth/SignUp.dart';
+import 'package:fitness_app/pages/mainPage.dart';
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatefulWidget {
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<OnBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          itemCount: 3,
+          itemBuilder: (_, i) {
+            return Stack(
+              children: [
+                Container(
+                  child: Image.asset("assets/images/Rectangle 1.png"),
+                ),
+                Positioned(
+                  bottom: 0,
+                  height: 400,
+                  width: 400,
+                  child: Container(
+                    width: 400, // Match the Positioned width
+                    height: 200, // Adjust the height as needed
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF0D1B2A), // Background color
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(
+                          20.0), // Add padding inside the container
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              "Where Strength is Tracked",
+                              style: TextStyle(
+                                color: Colors
+                                    .white, // Make text visible on dark background
+                                fontSize: 32,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          // Push the SignIn and Skip to the bottom
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment
+                                .center, // Align left and right
+
+                            children: [
+                              Center(
+                                child: GestureDetector(
+                                    onTap: () {
+                                      // Handle SignIn action
+                                      print("SignIn tapped");
+                                    },
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 50, vertical: 20)),
+                                      child: const Text(
+                                        "Sign In",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    SignIn()));
+                                      },
+                                    )),
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              GestureDetector(
+                                  onTap: () {
+                                    // Handle SignIn action
+                                    print("SignIn tapped");
+                                  },
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 50, vertical: 20)),
+                                    child: const Text(
+                                      "Sign Up",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => SignUp()));
+                                    },
+                                  )),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  // Handle Skip action
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => MainPage()));
+                                },
+                                child: const Text(
+                                  "Skip",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
